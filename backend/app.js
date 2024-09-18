a5346304938ed36681ae3c65cb473f03035c92a1
Shirke5445 <91075590+Shirke5445@users.noreply.github.com>
2024-09-18 14:39:43 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,35 @@
 import express from "express";
-const app = express();
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
 
 // Handle Uncaught exceptions
-process.on('uncaughtException', (err) =>{
-  console.log(`ERROR: ${err}`);
-  console.log("Shuting down due to uncaught exception");
+process.on('uncaughtException', (err) => {
+  console.error(`ERROR: ${err.message}`);
+  console.error("Shutting down due to uncaught exception");
   process.exit(1);
-});                                                         
+});
 
+const app = express();
+
+// Load environment variables
 dotenv.config({ path: "backend/config/config.env" });
 
+// Log the environment variables for debugging purposes
+console.log("Environment Variables:");
+console.log(`PORT: ${process.env.PORT}`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`DB_LOCAL_URI: ${process.env.DB_LOCAL_URI}`);
+console.log(`DB_URI: ${process.env.DB_URI}`);
+
 // Connecting to database
-connectDatabase()
-  
-app.use(express.json({ 
+connectDatabase();
+
+app.use(express.json({
   limit: "10mb",
   verify: (req, res, buf) => {
-    req.rawBody = buf.toString()
+    req.rawBody = buf.toString();
   },
 }));
 app.use(cookieParser());
@@ -31,30 +40,26 @@ import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
 
-
-//import orderRoutes from "./routes/order.js";  //crash
-
+// Set up API routes
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", paymentRoutes);
 
-//app.use("/api/v1", orderRoutes); //crash
-
-//Using Error Middleware
+// Using Error Middleware
 app.use(errorMiddleware);
 
-
-const server = app.listen(process.env.PORT, () => {
+// Start the server
+const server = app.listen(process.env.PORT || 4000, () => {
   console.log(
-    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+    `Server started on PORT: ${process.env.PORT || 4000} in ${process.env.NODE_ENV || 'DEVELOPMENT'} mode.`
   );
 });
 
-//Handele unhandle Promise rejection
+// Handle Unhandled Promise Rejections
 process.on("unhandledRejection", (err) => {
-  console.log(`ERROPR:${err}`);
-  console.log(`Shutting down server deu to Unhandle Promise Rejection`);
+  console.error(`ERROR: ${err.message}`);
+  console.error("Shutting down the server due to Unhandled Promise Rejection");
   server.close(() => {
     process.exit(1);
   });
